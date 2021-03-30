1ca1c625f48b612fd2afe85b7533fdcb4f0a8a80
IgorShum <Shumeikoigor@yandex.ru>
2021-03-30 22:15:02 +0300

    +Model Link +Tests

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_319_160_032) do
+
+ActiveRecord::Schema.define(version: 2021_03_30_190005) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -49,13 +50,22 @@ ActiveRecord::Schema.define(version: 20_210_319_160_032) do
     t.index ['user_id'], name: 'index_answers_on_user_id'
   end
 
-  create_table 'questions', force: :cascade do |t|
-    t.string 'title'
-    t.text 'body'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.bigint 'user_id'
-    t.index ['user_id'], name: 'index_questions_on_user_id'
+  create_table "links", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.bigint "question_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_links_on_question_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
   create_table 'users', force: :cascade do |t|
@@ -70,6 +80,7 @@ ActiveRecord::Schema.define(version: 20_210_319_160_032) do
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
-  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'answers', 'questions'
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "answers", "questions"
+  add_foreign_key "links", "questions"
 end
